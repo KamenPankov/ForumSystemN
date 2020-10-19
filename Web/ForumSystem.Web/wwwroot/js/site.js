75638d313b69dc06ef8a012ea8ab97d1c6553161
Kamen Pankov <kamen.pankov@gmail.com>
2020-10-19 20:04:42 +0300

    Convert all time to user's time mode.

--- Web/ForumSystem.Web/wwwroot/js/site.js
+++ Web/ForumSystem.Web/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(function() {
+    const timeElements = Array.from(document.querySelectorAll("time[datetime]"));
+    console.log(timeElements);
+    if (timeElements.length === 0) {
+        return;
+    }
+
+    timeElements.forEach(element => {
+        const time = moment.utc(element.getAttribute("datetime")).local();
+
+        element.textContent = time.format("llll");
+    })
+})
